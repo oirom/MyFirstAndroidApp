11bfea8452e5aa1cb143c3f56cec171aa364ecec
oirom <oirom0528@gmail.com>
2022-11-11 00:20:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myfirstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainMenu = findViewById<ListView>(R.id.mainMenu)
+        mainMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val item = parent.getItemAtPosition(position) as String
+            val show = "You tapped $item"
+            Toast.makeText(this@MainActivity, show, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
