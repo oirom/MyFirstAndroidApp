39337f3ee6b6eb414de93c403fa306c1944baf93
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-01-05 23:09:21 +0900

    Add screen transition with using Intent class

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -1,29 +1,50 @@
 package com.example.myfirstandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.SimpleAdapter
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // val mainMenu = findViewById<ListView>(R.id.mainMenu)
-        // val menuList = mutableListOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-        // val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val menuList: MutableList<MutableMap<String, String>> = mutableListOf()
+        var menu = mutableMapOf("name" to "Karage Teishoku", "price" to "$10")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "Sashimi Teishoku", "price" to "$20")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "Ramen Teishoku", "price" to "$30")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "Sukiyaki Teishoku", "price" to "$40")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "Steak Teishoku", "price" to "$50")
+        menuList.add(menu)
 
-        // mainMenu.adapter = adapter
-        // mainMenu.onItemClickListener = ListItemClickListener()
+        val from = arrayOf("name", "price")
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        val adapter = SimpleAdapter(this@MainActivity, menuList, android.R.layout.simple_list_item_2, from, to)
+        lvMenu.adapter = adapter
+
+        lvMenu.onItemClickListener = ListItemClickListener()
     }
 
     private inner class ListItemClickListener : AdapterView.OnItemClickListener {
         override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-            val dialogFragment = OrderConfirmDialogFragment()
-            dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
+            val item = parent.getItemAtPosition(position) as MutableMap<String, String>
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+            val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
+
+            intent2MenuThanks.putExtra("menuName", menuName)
+            intent2MenuThanks.putExtra("menuPrice", menuPrice)
+
+            startActivity(intent2MenuThanks)
         }
     }
 }
